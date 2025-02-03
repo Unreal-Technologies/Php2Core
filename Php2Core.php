4104035821965320facbb3c2f20012998ad1465d
Peter Overeijnder <peterovereijnder@hotmail.com>
2025-02-03 10:01:11 +0100

    -

--- Php2Core.php
+++ Php2Core.php
@@ -1,9 +1,11 @@
 <?php
 require_once('Php2Core/TServerAdminCommands.php');
+require_once('Php2Core/TDatabase.php');
 
 class Php2Core
 {
-    use Php2Core\Php2Core\TServerAdminCommands;
+    use \Php2Core\Php2Core\TServerAdminCommands;
+    use \Php2Core\Php2Core\TDatabase;
     
     /**
      * @return void
@@ -113,65 +115,7 @@ class Php2Core
             throw new \Exception('Route not found');
         }
     }
-    
-    /**
-     * @return void
-     */
-    private static function initializeDatabase(): void
-    {
-        $dbInfo1 = CONFIGURATION -> get('Database');
-        $dbInfo2 = CONFIGURATION -> get('CDatabase');
-        
-        $dbc1 = Php2Core\Db\Database::createInstance(TITLE, $dbInfo1['Host'], $dbInfo1['Username'], $dbInfo1['Password'], $dbInfo1['Database']);
-        $dbc2 = Php2Core\Db\Database::createInstance('Php2Core', $dbInfo2['Host'], $dbInfo2['Username'], $dbInfo2['Password'], $dbInfo2['Database']);
-        
-        if(!defined('Database'))
-        {
-            define('Database', [$dbc1, $dbc2]);
-        }
-        
-        self::initializeDatabaseOverride($dbc2, $dbInfo2);
-        self::initializeDatabaseOverride($dbc1, $dbInfo1);
-    }
-    
-    /**
-     * @param Php2Core\Db\Database $instance
-     * @param array $configuration
-     * @return void
-     * @throws \PDOException
-     */
-    private static function initializeDatabaseOverride(Php2Core\Db\Database $instance, array $configuration): void
-    {
-        $instance -> query('SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = \''.$configuration['Database'].'\'');
-        
-        try
-        {
-            $instance -> execute();
-        } 
-        catch (\PDOException $pex) 
-        {
-            if($pex -> getCode() === 1049)
-            {
-                $structureFile = realpath(str_replace(['{ROOT}', '{__DIR__}'], [ROOT, __DIR__], $configuration['Structure']));
-                $contentFile = realpath(str_replace(['{ROOT}', '{__DIR__}'], [ROOT, __DIR__], $configuration['Content']));
-                
-                if($structureFile !== false)
-                {
-                    $instance -> query(file_get_contents($structureFile));
-                    $instance -> execute(Php2Core\Db\Cache::CACHE_MEMORY, true);
-                }
-                
-                if($contentFile !== false)
-                {
-                    include($contentFile);
-                }
-                return;
-            }
-            throw $pex;
-        }
-        
-    }
-    
+
     /**
      * @return void
      */
---
+++ Php2Core/TDatabase.php
@@ -0,0 +1,62 @@
+<?php
+namespace Php2Core\Php2Core;
+
+trait TDatabase
+{
+    /**
+     * @return void
+     */
+    private static function initializeDatabase(): void
+    {
+        $dbInfo1 = CONFIGURATION -> get('Database');
+        $dbInfo2 = CONFIGURATION -> get('CDatabase');
+        
+        $dbc1 = \Php2Core\Db\Database::createInstance(TITLE, $dbInfo1['Host'], $dbInfo1['Username'], $dbInfo1['Password'], $dbInfo1['Database']);
+        $dbc2 = \Php2Core\Db\Database::createInstance('Php2Core', $dbInfo2['Host'], $dbInfo2['Username'], $dbInfo2['Password'], $dbInfo2['Database']);
+        
+        if(!defined('Database'))
+        {
+            define('Database', [$dbc1, $dbc2]);
+        }
+        
+        self::initializeDatabaseOverride($dbc2, $dbInfo2);
+        self::initializeDatabaseOverride($dbc1, $dbInfo1);
+    }
+    
+    /**
+     * @param \Php2Core\Db\Database $instance
+     * @param array $configuration
+     * @return void
+     * @throws \PDOException
+     */
+    private static function initializeDatabaseOverride(\Php2Core\Db\Database $instance, array $configuration): void
+    {
+        $instance -> query('SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = \''.$configuration['Database'].'\'');
+        
+        try
+        {
+            $instance -> execute();
+        } 
+        catch (\PDOException $pex) 
+        {
+            if($pex -> getCode() === 1049)
+            {
+                $structureFile = realpath(str_replace(['{ROOT}', '{__DIR__}'], [ROOT, __DIR__], $configuration['Structure']));
+                $contentFile = realpath(str_replace(['{ROOT}', '{__DIR__}'], [ROOT, __DIR__], $configuration['Content']));
+                
+                if($structureFile !== false)
+                {
+                    $instance -> query(file_get_contents($structureFile));
+                    $instance -> execute(Php2Core\Db\Cache::CACHE_MEMORY, true);
+                }
+                
+                if($contentFile !== false)
+                {
+                    include($contentFile);
+                }
+                return;
+            }
+            throw $pex;
+        }
+    }
+}
