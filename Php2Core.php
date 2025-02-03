92f29c32dfbb54307506f82c6debdc33003401b6
Peter Overeijnder <peterovereijnder@hotmail.com>
2025-02-03 10:04:13 +0100

    -

--- Php2Core.php
+++ Php2Core.php
@@ -1,11 +1,13 @@
 <?php
 require_once('Php2Core/TServerAdminCommands.php');
 require_once('Php2Core/TDatabase.php');
+require_once('Php2Core/TRouting.php');
 
 class Php2Core
 {
     use \Php2Core\Php2Core\TServerAdminCommands;
     use \Php2Core\Php2Core\TDatabase;
+    use \Php2Core\Php2Core\TRouting;
     
     /**
      * @return void
@@ -31,89 +33,6 @@ class Php2Core
     {
         $pi = pathinfo($_SERVER['SCRIPT_NAME']);
         return preg_replace('/'.substr($pi['dirname'], 1).'.+$/i', substr($pi['dirname'], 1), $_SERVER['SCRIPT_URI']);
-    }
-    
-    /**
-     * @param string $slug
-     * @return array
-     */
-    private static function getPossibleMatchesFromSlug(string $slug): array
-    {
-        $parts = explode('/', $slug);
-        $buffer = [ '^'.$parts[0].'$' ];
-        $offParts = [ $parts[0] ];
-        
-        for($i=1; $i<count($parts); $i++)
-        {
-            $offParts[$i] = '{.+}';
-            
-            $temp1 = implode('\\/', array_slice($parts, 0, $i));
-            $temp2 = implode('\\/', array_slice($offParts, 0, $i));
-            
-            $buffer[] = '^'.$temp1.'\\/'.$parts[$i].'$';
-            $buffer[] = '^'.$temp2.'\\/'.$parts[$i].'$';
-            
-            $buffer[] = '^'.$temp1.'\\/{.+}$';
-            $buffer[] = '^'.$temp2.'\\/{.+}$';
-        }
-        
-        return array_values(array_unique($buffer));
-    }
-    
-    /**
-     * @return void
-     * @throws \Exception
-     */
-    private static function initializeRouting(): void
-    {
-        //Get DB Instance
-        $coreDbc = Php2Core\Db\Database::getInstance('Php2Core');
-        $instanceId = 1;
-
-        //Get Default handler
-        $coreDbc -> query(
-                'select '
-                . 'case when `match` is null then \'index\' else `match` end as `match` '
-                . 'from `route` '
-                . 'where `default` = "true" '
-                . 'and '.(SERVER_ADMIN ? '( `instance-id` = '.$instanceId.' or `instance-id` is null )' : '`instance-id` = '.$instanceId).' '
-                . 'order by `id` asc '
-                . 'limit 0,1'
-        );
-        
-        $defaultResults = $coreDbc -> execute();
-        define('DEFAULT_ROUTE', $defaultResults['iRowCount'] === 0 ? 'index' : $defaultResults['aResults'][0]['match']);
-        
-        //Get Router Information
-        $router = new Php2Core\Router(DEFAULT_ROUTE);
-        $slug = $router -> slug();
-        $possibilities = self::getPossibleMatchesFromSlug($slug);
-        
-        //Get Possible routes
-        $coreDbc -> query(
-                'select '
-                . '`method`, `match`, `target`, `type` '
-                . 'from `route` '
-                . 'where '.(SERVER_ADMIN ? '( `instance-id` = '.$instanceId.' or `instance-id` is null )' : '`instance-id` = '.$instanceId).' '
-                . 'and (`match` regexp \''.implode('\' or `match` regexp \'', $possibilities).'\')'
-                . (SERVER_ADMIN ? '' : 'and `type` != \'function\' ')
-        );
-        
-        //Register possible routes
-        $routeResult = $coreDbc -> execute();
-        foreach($routeResult['aResults'] as $row)
-        {
-            $router -> register($row['method'].'::'.$row['match'], $row['type'].'#'.$row['target']);
-        }
-        
-        //get current route (if matched)
-        define('ROUTE', $router -> Match());
-        
-        //throw exception when not actually matched
-        if(ROUTE === null)
-        {
-            throw new \Exception('Route not found');
-        }
     }
 
     /**
