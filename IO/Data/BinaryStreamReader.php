0c1045742672280f0b1bd3cf0e2b2e0642f65575
Peter Overeijnder <peterovereijnder@hotmail.com>
2025-02-19 22:24:44 +0100

    -

--- IO/Data/BinaryStreamReader.php
+++ IO/Data/BinaryStreamReader.php
@@ -11,14 +11,14 @@ class BinaryStreamReader
     /**
      * @var string|null
      */
-    private ?string $sStream = null;
+    private static ?string $sStream = null;
     
     /**
      * @param string $stream
      */
     public function __construct(string $stream) 
     {
-        $this -> sStream = $stream;
+        self::$sStream = $stream;
         $this -> iPosition = 0;
     }
     
@@ -37,24 +37,27 @@ class BinaryStreamReader
      */
     public function read(int $length): string
     {
-        $data = substr($this -> sStream, $this -> iPosition, $length);
+        $data = substr(self::$sStream, $this -> iPosition, $length);
         $this -> iPosition += $length;
         return $data;
     }
-    
-	public function i64(): int
-	{
-		return unpack('q', $this -> read(8))[1];
-	}
-	
-	/**
-	 * @return string
-	 */
-	public function byte(): string
-	{
-		return unpack('C', $this -> read(1))[1];
-	}
-	
+
+    /**
+     * @return int
+     */
+    public function i64(): int
+    {
+        return unpack('q', $this -> read(8))[1];
+    }
+
+    /**
+     * @return int
+     */
+    public function u64(): int
+    {
+        return unpack('Q', $this -> read(8))[1];
+    }
+
     /**
      * @return int
      */
@@ -66,9 +69,17 @@ class BinaryStreamReader
     /**
      * @return int
      */
-    public function u64(): int
+    public function u32(): int
     {
-        return unpack('Q', $this -> read(8))[1];
+        return unpack('I', $this -> read(4))[1];
+    }
+    
+    /**
+     * @return int
+     */
+    public function i16(): int
+    {
+        return unpack('s', $this -> read(2))[1];
     }
     
     /**
@@ -78,15 +89,7 @@ class BinaryStreamReader
     {
         return unpack('S', $this -> read(2))[1];
     }
-    
-    /**
-     * @return int
-     */
-    public function u32(): int
-    {
-        return unpack('I', $this -> read(4))[1];
-    }
-    
+
     /**
      * @return bool
      */
@@ -101,7 +104,15 @@ class BinaryStreamReader
      */
     public function bytes(int $length): string
     {
-		return unpack('C', $this -> read($length))[1];
+        return $this -> read($length);
+    }
+    
+    /**
+     * @return string
+     */
+    public function byte(): string
+    {
+        return $this -> bytes(1);
     }
     
     /**
@@ -118,89 +129,5 @@ class BinaryStreamReader
     public function float(): float
     {
         return unpack('f', $this -> read(4))[1];
-    }
-    
-    /**
-     * @return string|null
-     */
-    public function optionalGuid(): ?string
-    {
-        $check = $this -> read(1);
-        if(ord($check) !== 0)
-        {
-            return $this -> guid();
-        }
-        return null;
-    }
-    
-    /**
-     * @return string
-     */
-    public function guid(): string
-    {
-        $data = $this -> read(16);
-        $b = [];
-        
-        for($i=0; $i<strlen($data); $i++)
-        {
-            $s = $data[$i];
-            $b[] = ord($s);
-        }
-
-        return sprintf(
-            '%08x-%04x-%04x-%04x-%04x%08x',
-            ($b[3] << 24) | ($b[2] << 16) | ($b[1] << 8) | $b[0],
-            ($b[7] << 8) | $b[6],
-            ($b[5] << 8) | $b[4],
-            ($b[0xb] << 8) | $b[0xa],
-            ($b[9] << 8) | $b[8],
-            ($b[0xf] << 24) | ($b[0xe] << 16) | ($b[0xd] << 8) | $b[0xc]
-        );
-    }
-    
-    /**
-     * @return string
-     */
-    public function fString(): string
-    {
-        $length = $this -> i32();
-        
-        if($length === 0)
-        {
-            return '';
-        }
-        
-        $encoding = null;
-        $data = null;
-        if($length < 0)
-        {
-            $length = -$length;
-            $data = substr($this -> read($length * 2), 0, -2);
-            $encoding = 'UTF-16LE';
-        }
-        else
-        {
-            $data = substr($this -> read($length), 0, -1);
-            $encoding = 'ascii';
-        }
-        
-        return mb_convert_encoding($data, 'UTF-8', $encoding);
-    }
-    
-    /**
-     * @param \Closure $closure
-     * @return array
-     */
-    public function tArray(\Closure $closure): array
-    {
-        $count = $this -> i32();
-        $buffer = [];
-        
-        for($i=0; $i<$count; $i++)
-        {
-            $buffer[] = $closure($this);
-        }
-        
-        return $buffer;
     }
 }
